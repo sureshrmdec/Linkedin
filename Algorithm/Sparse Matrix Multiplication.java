1c5c6b5d90711d3e765bcd40dc415b725d9369bf
xunhuanfengliuxiang <ruomingxxgame@gmail.com>
2017-03-16 16:51:59 -0400

    Update Sparse Matrix Multiplication.java

--- Algorithm/Sparse Matrix Multiplication.java
+++ Algorithm/Sparse Matrix Multiplication.java
@@ -63,46 +63,6 @@ public class Solution {
 
 
 
-
-
-
-public class Solution {
-    public int[][] multiply(int[][] A, int[][] B) {
-        
-        
-        if(A == null || A.length == 0 || A[0].length == 0){
-            return null;
-        }
-    
-        if(B == null || B.length == 0 || B[0].length == 0){
-            return null;
-        }
-        
-        int m = A.length;
-        int n = B[0].length;
-        
-        
-        int[][] res = new int[m][n];
-        
-        for(int i = 0; i < m; i++){
-            for(int k = 0; k < A[0].length; k++){
-                if(A[i][k] != 0){
-                    for(int j = 0; j < n; j++){
-                        res[i][j] += A[i][k] * B[k][j];
-                    }
-                }
-            }
-        }
-        
-        return res;
-    }
-}
-
-
-
-
-
-
     public static void main(String[] args) {
         // TODO Auto-generated method stub
         DotProduct sol = new DotProduct();
