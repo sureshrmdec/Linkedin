69e406212985b0326e7c0c6188910c2ea0e44fc5
xunhuanfengliuxiang <ruomingxxgame@gmail.com>
2017-03-18 00:45:03 -0400

    Update Find Leaves of Binary Tree.java

--- Algorithm/Find Leaves of Binary Tree.java
+++ Algorithm/Find Leaves of Binary Tree.java
@@ -33,6 +33,15 @@ Returns [4, 5, 3], [2], [1].
  *     TreeNode(int x) { val = x; }
  * }
  */
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
 public class Solution {
     public List<List<Integer>> findLeaves(TreeNode root) {
         
@@ -58,7 +67,7 @@ public class Solution {
         
         int lev = 1 + Math.max(helper(root.left, res), helper(root.right, res));
         
-        if(res.size() < lev + 1){
+        if(res.size() == lev){
             res.add(new ArrayList<Integer>());
         }
         res.get(lev).add(root.val);
