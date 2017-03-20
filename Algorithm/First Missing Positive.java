ec17771b887cee2c7edb6c7b9d11f0771fb2eb63
xunhuanfengliuxiang <ruomingxxgame@gmail.com>
2017-03-19 23:51:04 -0400

    Create First Missing Positive.java

---
+++ Algorithm/First Missing Positive.java
@@ -0,0 +1,43 @@
+/*
+Given an unsorted integer array, find the first missing positive integer.
+
+For example,
+Given [1,2,0] return 3,
+and [3,4,-1,1] return 2.
+
+Your algorithm should run in O(n) time and uses constant space.
+*/
+
+public class Solution {
+    public int firstMissingPositive(int[] nums) {
+        
+        
+        if(nums == null || nums.length == 0){
+            return 1;
+        }
+        
+        int i = 0; 
+        
+        while(i < nums.length){
+            if(nums[i] > 0 && nums[i] <= nums.length  && nums[i] != nums[nums[i] - 1]){
+                swap(nums, i, nums[i] - 1);
+            }else{
+                i++;
+            }
+        }
+        
+        for(int j = 0; j < nums.length; j++){
+            if(nums[j] != j + 1){
+                return j + 1;
+            }
+        }
+        
+        return nums.length + 1;
+    }
+    
+    private void swap(int[] nums, int i , int j){
+        int temp = nums[i];
+        nums[i] = nums[j];
+        nums[j] = temp;
+    }
+}
