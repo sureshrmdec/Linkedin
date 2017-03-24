7a9ea40c3bea1c7ac3ad208c7ac239a880f25415
xunhuanfengliuxiang <ruomingxxgame@gmail.com>
2017-03-24 16:27:13 -0400

    Update Convex Polygon.java

--- Algorithm/Convex Polygon.java
+++ Algorithm/Convex Polygon.java
@@ -27,6 +27,47 @@ Show Tags
 
 */
 
+    public boolean isConvex(int[][] points) {
+
+        if (points == null || points.length == 0 || points[0].length == 0) {
+
+            return false;
+        }
+
+
+        int curr = 0;
+        int prev = 0;
+
+        int n = points.length;
+
+        for (int i = 0; i < n; i++) {
+
+            int dx1 = (points[(i + 1) % n][0]) - (points[(i) % n][0]);
+            int dy1 = (points[(i + 1) % n][1]) - (points[(i) % n][1]);
+            int dx2 = (points[(i + 2) % n][0]) - (points[(i) % n][0]);
+            int dy2 = (points[(i + 2) % n][1]) - (points[(i) % n][1]);
+
+
+           curr = dx1 * dy2 - dx2 * dy1;
+
+           if(curr != 0){
+
+               if(curr * prev < 0){
+                   return false;
+               }
+
+
+               prev = curr;
+
+           }
+        }
+
+        return true;
+
+    }
+
+
+
 public class Solution {
     public boolean isConvex(List<List<Integer>> points) {
         
