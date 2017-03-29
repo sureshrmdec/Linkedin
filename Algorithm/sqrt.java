52ffd2d6697d9dac1c1fbf177cf8909c3f39ce45
xunhuanfengliuxiang <ruomingxxgame@gmail.com>
2017-03-29 17:36:24 -0400

    Update sqrt.java

--- Algorithm/sqrt.java
+++ Algorithm/sqrt.java
@@ -58,3 +58,56 @@ double sqrt(double x) {
     }
     return res;
 }
+
+public class SquareRootFinder {
+ 
+    public static void main(String args[]) {
+ 
+        System.out.println("Square Root of 25 is: " + squareRoot(25));
+        System.out.println("Square Root of 81 is: " + squareRoot(81));
+        System.out.println("Square Root of -100 is: " + squareRoot(-100));
+        System.out.println("Square Root of 1 is: " + squareRoot(1));
+        System.out.println("Square Root of 0 is: " + squareRoot(0));
+ 
+    }
+ 
+    /**
+     * This method use binary search to find the square root.
+     *
+     * @param number
+     * @return square root of the number
+     */
+    private static double squareRoot(double number) {
+        double squareRoot = 0;
+        double startValue = 0;
+        double endValue = number;
+        double precision = 0.00001;
+ 
+        if (number < 0) {
+            squareRoot = -1;
+        } else if (number == 0 || number == 1) {
+            squareRoot = number;
+        } else {
+ 
+            // we will use binary search to narrow down.
+            while (endValue - startValue > precision) {
+                double midValue = (startValue + endValue) / 2;
+                squareRoot = midValue * midValue;
+ 
+                if (squareRoot == number) {
+                    return squareRoot;
+                } else if (squareRoot > number) {
+                    endValue = midValue;
+                } else {
+                    startValue = midValue;
+                }
+            }
+ 
+            // if a match is not found
+            squareRoot = (startValue + endValue) / 2;
+ 
+        }
+        return squareRoot;
+    }
+ 
+}
